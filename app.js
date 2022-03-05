dd315e382386fa5cbdf6855b1c9669ba91eb2cc1
D05S <dominiksauer76@gmail.com>
2022-03-05 10:12:18 +0100

    update

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
 
 const app = express();
 
@@ -30,6 +31,44 @@ app.post("/", function(req, res){
     let formInformation = [formName, formSurname, formEmail, formDate, formText,];   
 
     console.log(formInformation);
+
+    
+    async function main() {
+        // Generate test SMTP service account from ethereal.email
+        // Only needed if you don't have a real mail account for testing
+        let testAccount = await nodemailer.createTestAccount();
+      
+        // create reusable transporter object using the default SMTP transport
+        let transporter = nodemailer.createTransport({
+          host: "smtp.ethereal.email",
+          port: 587,
+          secure: false, // true for 465, false for other ports
+          auth: {
+            user: testAccount.user, // generated ethereal user
+            pass: testAccount.pass, // generated ethereal password
+          },
+        });
+      
+        // send mail with defined transport object
+        let info = await transporter.sendMail({
+          from: '"Fred Foo 👻" <foo@example.com>', // sender address
+          to: "dominiksauer76@gmail.com", // list of receivers
+          subject: " Svatební kameraman ", // Subject line
+          jmeno: formName, formSurname,
+          datum: formDate,
+          text:formText, // plain text body
+          html: "<b>Hello world?</b>", // html body
+        });
+      
+        console.log("Message sent: %s", info.messageId);
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+      
+        // Preview only available when sending through an Ethereal account
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+      }
+      
+      main().catch(console.error);
 
     res.redirect("/");
 });
